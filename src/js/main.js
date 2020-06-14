25d91554a6e0a3797829a6abda5d9dd2926a1669
Vlada <vlada_chvanova@mail.ru>
2020-06-14 22:20:26 +0300

    начала типы данных(js)

---
+++ src/js/main.js
@@ -0,0 +1,20 @@
+// 1. Переменные
+// var name = 'Vlada' // string-строчный тип данных, стараться не использовать VAR
+// camelCase- написание 1-го слова с маленькой буквы, последующие слова с большой без пробелов 
+const firstName = 'Vlada'
+// const lastName = 'Chvanova' // string-строчный тип данных
+let age = 20 // number-числовой тип данных
+const isProgrammer = true // boolean- булевый тип данных
+
+
+// Валидное(ый)- корректный код, название и т.д.
+const withNum5 = '5'
+// const if = 'mkef' // err- нельзя называть переменные зарезервированными словами
+//  нельзя ставить цифру в начале названия переменной. Пример const 5wihNum = '5'
+// 2 Типы данных (Мутирование)
+console.log('Имя человека: ' + firstName + ', а возраст человека: ' + age)
+
+const name = 'vlada'
+alert(name)
+const lastName = prompt('Введите фамилию')
+alert(firstName + ' ' + lastName)
