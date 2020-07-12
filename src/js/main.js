c649d31cd37c39760685a2c6fed4bcb5cdcd573e
Vlada <vlada_chvanova@mail.ru>
2020-07-12 19:28:51 +0300

    закончили с Number, переходим к String

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-// Number
+// 1 Number
 // const num = 42; // integer
 // const float = 42.42; // float
 // const pow = 10e3;
@@ -28,3 +28,32 @@
 // console.log(0.4 + 0.2);
 // console.log(2 / 5 + 1 / 5);
 // console.log(+(0.4 + 0.2).toFixed(1));
+
+// 2 BigInt - тип данных- нужен для того, чтобы работать с числами > MAX_SAFE_INTEGER
+// console.log(typeof 90071992547409919999999n);
+// console.log(90071992547409919999999n - 9007199254740991999999n); // операции можно проводить только если все элементы являются BigInt(ами)
+// console.log(-90071992547409919999999n);
+// // console.log(-90071992547409919999999.66n); //error -десятичные значения
+// // console.log(10n - 4) // error, нельзя делать операции с разными типами данных
+// console.log(parseInt(10n) - 4); // но можно преобразовать BigInt в Number
+// // console.log(10n - BigInt(4)); // или Number в BigInt
+
+// 3 Math
+// console.log(Math.E);
+// console.log(Math.PI);
+// console.log(Math.sqrt(25)); //КВАДРАТНЫЙ КОРЕНЬ ИЗ 25 = 5
+// console.log(Math.pow(5, 2)); // ВРЗВЕСТИ В СТЕПЕНЬ
+// console.log(Math.abs(-42)); // МОДУЛЬ ЧИСЛА
+// console.log(Math.max(42, 12, 23, 11, 422)); // ВЫВОДИТ МАКСИМАЛЬНОЕ ЗНАЧЕНИЕ ИЗ ПРЕДСТАВЛЕННЫХ
+// console.log(Math.min(42, 12, 23, 11, 422)); // ВЫВОДИТ МИНИМАЛЬНОЕ ЗНАЧЕНИЕ ИЗ ПРЕДСТАВЛЕННЫХ
+// console.log(Math.floor(4.9)); // ОКРУГЛЯЕТ ЗНАЧЕНИЯ В МЕНЬШУЮ СТОРОНУ
+// console.log(Math.ceil(4.9)); // ОКРУГЛЯЕТ ЗНАЧЕНИЯ В БОЛЬШУЮ СТОРОНУ
+// console.log(Math.round(4.9)); // ОКРУГЛЯЕТ ЗНАЧЕНИЯ К БЛИЖАЙШЕМУ ЦЕЛОМУ ЧИСЛУ
+// console.log(Math.trunc(4.9)); // ВОЗВРАЩАЕТ ЦЕЛОЕ ЧИСЛО ПУТЕМ УДАЛЕНИЯ ДРОБНОЙ ЧАСТИ
+// console.log(Math.random()); // НЕ ПРИНИМАЕТ ПАРАМЕТРОВ, НО ВЫДАЕТ РАНДОМНОЕ ЗНАЧЕНИЕ ЧИСЛА
+
+// // 4 EXEMPLE
+// function getRandomBetween(min, max) {
+//   return Math.floor(Math.random() * (max - min + 1) + min);
+// }
+// console.log(getRandomBetween(10, 42));
