2df557b6214102cad2d9e94bbd8964ef5e16fa67
Vlada <vlada_chvanova@mail.ru>
2020-07-12 16:34:29 +0300

    узнала про максимальные и минимальные значения, parseInt -Float немного еще

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,26 @@
 // Number
-const num = 42 // integer
-const float = 42.42 // float
-const pow = 10e3
+// const num = 42; // integer
+// const float = 42.42; // float
+// const pow = 10e3;
 
-console.log(pow)
+// console.log("MAX_SAFE_INTEGER", Number.MAX_SAFE_INTEGER);
+// console.log("Math.pow 53", Math.pow(2, 53) - 1);
+// console.log("MIN_SAFE_INTEGER", Number.MAX_SAFE_INTEGER);
+// console.log("MAX_VALUE", Number.MAX_VALUE);
+// console.log("MIN_VALUE", Number.MIN_VALUE);
+// console.log("POSITIVE_INFINITY", Number.POSITIVE_INFINITY);
+// console.log("NEGATIVE_INFINITY", Number.NEGATIVE_INFINITY);
+// console.log("2 / 0", 2 / 0);
+// console.log(Number.NaN);
+// console.log(typeof NaN);
+// const weird = 2 / undefined;
+// console.log(Number.isNaN(weird));
+// console.log(Number.isNaN(42));
+// console.log(isFinite(Infinity));
+// console.log(isFinite(42));
+
+// const stringInt = "40";
+// const stringFloat = "40.42";
+// console.log(parseInt(stringInt) + 2);
+// console.log(parseFloat(stringFloat) + 2);
+// console.log(+stringFloat + 2); // знак '+' заменяет parseInt -Float, преобразует строку в число
